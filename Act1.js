c25d0481315badd77a00cdb78d39b6688fdba56e
charliejgy4 <103393876+charliejgy4@users.noreply.github.com>
2022-10-16 20:26:34 +0100

    Add files via upload

---
+++ Act1.js
@@ -0,0 +1,89 @@
+// Activity 1
+// let Name    =   "Charlie";
+
+// let age     =   "40";
+
+// let favcolor=   "blue"
+
+// console.log ("Hi, my name"+ Name    + "I am"+   age + "and my favorit drink is"+    favcolor    +   ".")
+
+// age = 45;
+
+// favcolor = "red"
+
+// console.log (`Hi, my Name ${Name}  I am  ${age}  and my favorit drink is ${favcolor}.`)
+// 
+
+
+// Activity 5
+
+// let num = 15;
+// if (num/"3") {
+//   console.log("Buzz.");
+// } else if (num/"3"/ "5") {
+//   Console.log("Buzzfuzz.");
+// } else{
+//     console.log ("num");
+// }
+// out put Buzz
+
+
+
+// Movie
+
+
+//   let favFilms = [
+
+//     "Black"   - "Volvo",
+//     "Saab"  - "Ford",
+//     "Fiat" - "Audi",
+//     "Love" - "Water",
+//     "Peace"-"Still"
+//     ]
+// for(let i =0;i<favFilms.length; i++){
+// console.log(favFilms[i])
+
+// }
+
+
+
+//   let text = "";
+//   for (let i = 0; i 
+//     < cars.length; i++) {
+//     text += cars[i] + "<br>";
+//     console.log ("cars");
+//   }
+
+// Loop
+
+
+//   let i = ["0,1,2,3 4, 5, 6, 7, 8,9;"]
+
+// for (let i = 0; i < 9; i++) {
+  
+//     console.log (i);
+
+// }
+
+
+
+// // Days until birthday
+
+// let today = new Date();
+// let birthday=new Date(today.getFullYear(), 10, 14);
+// if (today.getMonth()==10 && today.getDate()>14) 
+// {
+// birthday.setFullYear(birthday.getFullYear()+1); 
+// }  
+// {let one_day=1000*60*60*24;
+// console.log(Math.ceil((birthday.getTime()-today.getTime())/(one_day))+
+
+// today=new Date();
+// const birthday=new Date(today.getFullYear(), 06, 06);
+// if (today.getMonth()==06 && today.getDate()>06) 
+// {
+// birthday.setFullYear(birthday.getFullYear()+1); 
+// }  
+// const one_day=1000*60*50*12;
+// console.log(`${Math.ceil((birthday.getTime()-today.getTime())/(one_day))} days left until birthday!`);
+
